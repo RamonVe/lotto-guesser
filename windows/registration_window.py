0a6f8c26a5770e27f54a291884cb30a43dcfc80c
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-01-29 02:19:14 -0600

    LG-1: -Added user object variables. -Created new register method to take entry variables and create and save a user object for registration.

--- windows/registration_window.py
+++ windows/registration_window.py
@@ -1,11 +1,11 @@
-import tkinter
-from geopy.geocoders import Nominatim
+from entities import user
+from storage import local_data_utillities
 from tkinter import *
 from tkinter import messagebox
+import tkinter
 
 
 def registration_window(root_window, login_window):
-
     login_window.destroy()
 
     register_window = tkinter.Toplevel(root_window)
@@ -19,12 +19,19 @@ def registration_window(root_window, login_window):
     user_name_label = Label(register_window, text='User Name')
     pass_word_label = Label(register_window, text='Password')
 
-    f_name_entry = Entry(register_window)
-    l_name_entry = Entry(register_window)
-    age_entry = Entry(register_window)
-    location_entry = Entry(register_window)
-    user_name_entry = Entry(register_window)
-    pass_word_entry = Entry(register_window)
+    f_name = StringVar()
+    l_name = StringVar()
+    age = StringVar()
+    location = StringVar()
+    user_name = StringVar()
+    pass_word = StringVar()
+
+    f_name_entry = Entry(register_window, textvariable=f_name)
+    l_name_entry = Entry(register_window, textvariable=l_name)
+    age_entry = Entry(register_window, textvariable=age)
+    location_entry = Entry(register_window, textvariable=location)
+    user_name_entry = Entry(register_window, textvariable=user_name)
+    pass_word_entry = Entry(register_window, textvariable=pass_word)
 
     f_name_label.grid(row=0, column=0)
     l_name_label.grid(row=1, column=0)
@@ -40,10 +47,27 @@ def registration_window(root_window, login_window):
     user_name_entry.grid(row=4, column=1)
     pass_word_entry.grid(row=5, column=1)
 
-    Button(register_window, text='Submit', width=16, bg='black', fg='white').grid(row=6, column=1)
+    Button(register_window, text='Submit', width=16, bg='black', fg='white',
+           command=lambda: register(f_name,
+                                    l_name,
+                                    age,
+                                    location,
+                                    user_name,
+                                    pass_word)).grid(row=6, column=1)
 
     def close():
         if messagebox.askokcancel('Quit', 'Do You Want To Quit?'):
             root_window.destroy()
 
     register_window.protocol('WM_DELETE_WINDOW', close)
+
+
+def register(f_name, l_name, age, location, user_name, pass_word):
+    new_user = user.User(first_name=f_name.get(),
+                         last_name=l_name.get(),
+                         age=age.get(),
+                         location=location.get(),
+                         username=user_name.get(),
+                         password=pass_word.get())
+
+    local_data_utillities.save_object(new_user)
