ef5ffab04520556d9c19d9f3c761f0b35b35604f
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-01-29 16:46:53 -0600

    LG-1: -Added return to login window button and functionality. -Removed register button black color. -Added punctuation to some strings.

--- windows/registration_window.py
+++ windows/registration_window.py
@@ -48,7 +48,7 @@ def registration_window(root_window, log_window):
     user_name_entry.grid(row=4, column=1)
     pass_word_entry.grid(row=5, column=1)
 
-    Button(register_window, text='Submit', width=16, bg='black', fg='white',
+    Button(register_window, text='Submit', width=16,
            command=lambda: register(root_window,
                                     register_window,
                                     f_name,
@@ -58,8 +58,11 @@ def registration_window(root_window, log_window):
                                     user_name,
                                     pass_word)).grid(row=6, column=1)
 
+    Button(register_window, text='Return To Login', width=16,
+           command=lambda: return_to_login(root_window, register_window)).grid(row=7, column=1)
+
     def close():
-        if messagebox.askokcancel('Quit', 'Do You Want To Quit?'):
+        if messagebox.askokcancel('Quit Lotto Guesser?', 'Do You Want To Quit?'):
             root_window.destroy()
 
     register_window.protocol('WM_DELETE_WINDOW', close)
@@ -80,20 +83,25 @@ def register(root_window, register_window, f_name, l_name, age, location, user_n
                          username=user_name_input,
                          password=pass_word_input)
 
-    if first_name_input == '' or last_name_input == '' or age_input == '' or location_input == ''\
+    if first_name_input == '' or last_name_input == '' or age_input == '' or location_input == '' \
             or user_name_input == '' or pass_word_input == '':
-        messagebox.showwarning('Empty Field', 'A field has been left empty!')
+        messagebox.showwarning('Empty Field!', 'A field has been left empty!')
         return
 
     users = local_data_utilities.load_users()
 
     for exiting_user in users:
         if exiting_user.username == user_name.get():
-            messagebox.showwarning('Duplicate Username.', 'The username you entered has already been taken!')
+            messagebox.showwarning('Duplicate Username!', 'The username you entered has already been taken!')
             return
 
     local_data_utilities.save_user(new_user)
-    messagebox.showinfo('Success', user_name_input + ' has been successfully registered')
+    messagebox.showinfo('Success!', user_name_input + ' has been successfully registered!')
 
+    register_window.destroy()
+    login_window.login_window(root_window)
+
+
+def return_to_login(root_window, register_window):
     register_window.destroy()
     login_window.login_window(root_window)
