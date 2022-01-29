287002125e0f5e1081773d40bb5e50d63e409ff9
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-01-29 05:20:21 -0600

    LG-1: -Implemented user class to use first name for dashboard window name.

--- windows/dashboard_window.py
+++ windows/dashboard_window.py
@@ -1,15 +1,16 @@
 import tkinter
+from entities import user
 from tkinter import *
 from tkinter import messagebox
 
 
-def dashboard_window(root_window):
+def dashboard_window(root_window, logged_in_user):
 
     dash_window = tkinter.Toplevel(root_window)
 
     dash_window.geometry('500x500')
 
-    dash_window.title('Dashboard')
+    dash_window.title(user.User.first_name(logged_in_user) + "'s Dashboard")
 
     def close():
         if messagebox.askokcancel('Quit', 'Do You Want To Quit?'):
