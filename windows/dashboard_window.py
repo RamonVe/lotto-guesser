05af40cbb73a0b1f255346adb41982e620d72b69
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-01-29 22:19:33 -0600

    LG-2: -Resized window. -Added buttons for practice and future test. -Implemented quit confirmation function. -Created practice test function to launch practice test window for logged in user.

--- windows/dashboard_window.py
+++ windows/dashboard_window.py
@@ -1,19 +1,25 @@
 from entities import user
 from tkinter import *
-from tkinter import messagebox
+from windows.test_windows import practice_test_window
+from windows.window_utillities import window_protocol
 import tkinter
 
 
 def dashboard_window(root_window, logged_in_user):
-
     dash_window = tkinter.Toplevel(root_window)
 
-    dash_window.geometry('500x500')
+    dash_window.geometry('450x200')
 
     dash_window.title(user.User.first_name(logged_in_user) + "'s Dashboard")
 
-    def close():
-        if messagebox.askokcancel('Quit', 'Do You Want To Quit?'):
-            root_window.destroy()
+    Button(dash_window, text='Start A Practice Test', width=20,
+           command=lambda: practice_test(root_window, dash_window, logged_in_user)).grid(row=0, column=0)
+    Label(dash_window, text='', width=20).grid(row=0, column=1)
+    Button(dash_window, text='Start A Future Test', width=20).grid(row=0, column=2)
 
-    dash_window.protocol('WM_DELETE_WINDOW', close)
+    window_protocol.quit_confirmation(root_window, dash_window)
+
+
+def practice_test(root_window, dash_window, logged_in_user):
+    dash_window.destroy()
+    practice_test_window.practice_test_window(root_window, logged_in_user)
