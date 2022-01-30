0df850fcc13c391ace015e2ecbe2963031158a74
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-01-29 22:11:00 -0600

    LG-2: -Improved messaging.

--- windows/window_utillities/window_protocol.py
+++ windows/window_utillities/window_protocol.py
@@ -3,7 +3,8 @@ from tkinter import messagebox
 
 def quit_confirmation(root_window, current_window):
     def close():
-        if messagebox.askokcancel('Quit Lotto Guesser?', 'Do You Want To Quit?'):
+        if messagebox.askokcancel('Quit Lotto Guesser?',
+                                  'Do you want to close the program?' + '\n' + 'Any unsaved progress will be lost.'):
             root_window.destroy()
 
     current_window.protocol('WM_DELETE_WINDOW', close)
