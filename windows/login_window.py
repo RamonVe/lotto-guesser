e55bb49b44c3189b4d03665bf91f3b5263b6eff5
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-01-29 22:12:01 -0600

    LG-2: -Reorganized functions, and implemented window protocol function.

--- windows/login_window.py
+++ windows/login_window.py
@@ -3,27 +3,8 @@ from tkinter import messagebox
 from storage import local_data_utilities
 from windows import dashboard_window
 from windows import registration_window
+from windows.window_utillities import window_protocol
 import tkinter
-
-
-def validate_login(user_name, pass_word, root_window, log_window):
-    if user_name.get() == '' and pass_word.get() == '':
-        messagebox.showwarning('Empty Fields!', 'Enter a username and password to log in!')
-
-    users = local_data_utilities.load_users()
-
-    for user in users:
-        if user.username == user_name.get():
-            if user.password != pass_word.get():
-                messagebox.showwarning('Wrong Password!', 'You have entered the wrong password, try again!')
-            else:
-                log_window.destroy()
-                dashboard(root_window, user)
-                break
-
-
-def dashboard(root_window, logged_in_user):
-    dashboard_window.dashboard_window(root_window, logged_in_user)
 
 
 def login_window(root_window):
@@ -47,8 +28,24 @@ def login_window(root_window):
            command=lambda: registration_window.registration_window(root_window, log_window)).grid(row=4,
                                                                                                   column=1)
 
-    def close():
-        if messagebox.askokcancel('Quit', 'Do You Want To Quit?'):
-            root_window.destroy()
+    window_protocol.quit_confirmation(root_window, log_window)
 
-    log_window.protocol('WM_DELETE_WINDOW', close)
+
+def validate_login(user_name, pass_word, root_window, log_window):
+    if user_name.get() == '' and pass_word.get() == '':
+        messagebox.showwarning('Empty Fields!', 'Enter a username and password to log in!')
+
+    users = local_data_utilities.load_users()
+
+    for user in users:
+        if user.username == user_name.get():
+            if user.password != pass_word.get():
+                messagebox.showwarning('Wrong Password!', 'You have entered the wrong password, try again!')
+            else:
+                log_window.destroy()
+                dashboard(root_window, user)
+                break
+
+
+def dashboard(root_window, logged_in_user):
+    dashboard_window.dashboard_window(root_window, logged_in_user)
