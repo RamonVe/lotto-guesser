1c210f0f91dec6d1e09129354c6813a11afbdf60
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-01 03:32:05 -0600

    LG-2: -Renamed varaibled, and shortened import name.

--- windows/test_windows/practice_test/practice_test_window.py
+++ windows/test_windows/practice_test/practice_test_window.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from windows import dashboard_window
+from windows import dashboard_window as dw
 from windows.test_windows.practice_test import practice_test_session_input_window
 from windows.window_utillities import window_icon
 from windows.window_utillities import window_protocol
@@ -7,32 +7,32 @@ import tkinter
 
 
 def practice_test_window(root_window, logged_in_user):
-    p_test_window = tkinter.Toplevel(root_window)
-    p_test_window.geometry('320x150')
-    p_test_window.title('Practice Test')
-    p_test_window.iconbitmap(window_icon.window_icon())
+    practice_window = tkinter.Toplevel(root_window)
+    practice_window.geometry('320x150')
+    practice_window.title('Practice Test')
+    practice_window.iconbitmap(window_icon.window_icon())
 
-    test_label = Label(p_test_window, text='Select a lottery to practice.')
+    test_label = Label(practice_window, text='Select a lottery to practice.')
 
-    power_ball_button = Button(p_test_window, text='Powerball',
-                               command=lambda: power_ball(root_window, p_test_window, logged_in_user))
+    power_ball_button = Button(practice_window, text='Powerball',
+                               command=lambda: power_ball(root_window, practice_window, logged_in_user))
 
-    empty_space_one = Label(p_test_window, text='')
+    empty_space_one = Label(practice_window, text='')
 
-    mega_millions_button = Button(p_test_window, text='Mega Millions',
-                                  command=lambda: mega_millions(root_window, p_test_window, logged_in_user))
-    lotto_america_button = Button(p_test_window, text='Lotto America',
-                                  command=lambda: lotto_america(root_window, p_test_window, logged_in_user))
+    mega_millions_button = Button(practice_window, text='Mega Millions',
+                                  command=lambda: mega_millions(root_window, practice_window, logged_in_user))
+    lotto_america_button = Button(practice_window, text='Lotto America',
+                                  command=lambda: lotto_america(root_window, practice_window, logged_in_user))
 
-    empty_space_two = Label(p_test_window, text='')
+    empty_space_two = Label(practice_window, text='')
 
-    cash_four_life_button = Button(p_test_window, text='Cash 4 Life',
-                                   command=lambda: cash_four_life(root_window, p_test_window, logged_in_user))
-    tn_cash_button = Button(p_test_window, text='TN Cash',
-                            command=lambda: tn_cash(root_window, p_test_window, logged_in_user))
+    cash_four_life_button = Button(practice_window, text='Cash 4 Life',
+                                   command=lambda: cash_four_life(root_window, practice_window, logged_in_user))
+    tn_cash_button = Button(practice_window, text='TN Cash',
+                            command=lambda: tn_cash(root_window, practice_window, logged_in_user))
 
-    dashboard_button = Button(p_test_window, text='Return To Dashboard',
-                              command=lambda: return_to_dash_board(root_window, p_test_window, logged_in_user))
+    dashboard_button = Button(practice_window, text='Return To Dashboard',
+                              command=lambda: dash_board(root_window, practice_window, logged_in_user))
 
     test_label.grid(row=0, column=1)
     power_ball_button.grid(row=1, column=0)
@@ -44,34 +44,34 @@ def practice_test_window(root_window, logged_in_user):
     tn_cash_button.grid(row=4, column=1)
     dashboard_button.grid(row=5, column=1)
 
-    window_protocol.quit_confirmation(root_window, p_test_window)
+    window_protocol.quit_confirmation(root_window, practice_window)
 
 
-def power_ball(root_window, p_test_window, logged_in_user):
-    practice_test_input(root_window, p_test_window, 'Powerball', logged_in_user)
+def power_ball(root_window, practice_window, logged_in_user):
+    practice_test_input(root_window, practice_window, 'Powerball', logged_in_user)
 
 
-def mega_millions(root_window, p_test_window, logged_in_user):
-    practice_test_input(root_window, p_test_window, 'Mega Millions', logged_in_user)
+def mega_millions(root_window, practice_window, logged_in_user):
+    practice_test_input(root_window, practice_window, 'Mega Millions', logged_in_user)
 
 
-def lotto_america(root_window, p_test_window, logged_in_user):
-    practice_test_input(root_window, p_test_window, 'Lotto America', logged_in_user)
+def lotto_america(root_window, practice_window, logged_in_user):
+    practice_test_input(root_window, practice_window, 'Lotto America', logged_in_user)
 
 
-def cash_four_life(root_window, p_test_window, logged_in_user):
-    practice_test_input(root_window, p_test_window, 'Cash 4 Life', logged_in_user)
+def cash_four_life(root_window, practice_window, logged_in_user):
+    practice_test_input(root_window, practice_window, 'Cash 4 Life', logged_in_user)
 
 
-def tn_cash(root_window, p_test_window, logged_in_user):
-    practice_test_input(root_window, p_test_window, 'TN Cash', logged_in_user)
+def tn_cash(root_window, practice_window, logged_in_user):
+    practice_test_input(root_window, practice_window, 'TN Cash', logged_in_user)
 
 
-def practice_test_input(root_window, p_test_window, selected_lottery, logged_in_user):
-    p_test_window.destroy()
-    practice_test_session_input_window.practice_test_session_input_window(root_window, selected_lottery, logged_in_user)
+def practice_test_input(root_window, practice_window, selected_lottery, logged_in_user):
+    practice_window.destroy()
+    practice_test_session_input_window.practice_session_input_window(root_window, selected_lottery, logged_in_user)
 
 
-def return_to_dash_board(root_window, p_test_window, logged_in_user):
-    p_test_window.destroy()
-    dashboard_window.dashboard_window(root_window, logged_in_user)
+def dash_board(root_window, practice_window, logged_in_user):
+    practice_window.destroy()
+    dw.dashboard_window(root_window, logged_in_user)
