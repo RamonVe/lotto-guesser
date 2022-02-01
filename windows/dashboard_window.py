0317b622bd052e5a3a4195c2535db5d6be7f193a
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-01 04:05:57 -0600

    LG-2: -Optimized naming of variables and imports.

--- windows/dashboard_window.py
+++ windows/dashboard_window.py
@@ -1,25 +1,26 @@
-from entities import user
+from entities import user as u
 from tkinter import *
-from windows.test_windows.practice_test import practice_test_window
-from windows.window_utillities import window_icon
-from windows.window_utillities import window_protocol
+from windows.test_windows.practice_test import practice_test_home_window as pth
+from windows.window_utillities import window_icon as wi
+from windows.window_utillities import window_protocol as wp
 import tkinter
 
 
-def dashboard_window(root_window, logged_in_user):
+def dashboard_window(root_window, user):
     dash_window = tkinter.Toplevel(root_window)
     dash_window.geometry('450x200')
-    dash_window.title(user.User.first_name(logged_in_user) + "'s Dashboard")
-    dash_window.iconbitmap(window_icon.window_icon())
+    first_name = u.User.first_name(user)
+    dash_window.title(first_name + "'s Dashboard")
+    dash_window.iconbitmap(wi.window_icon())
 
     Button(dash_window, text='Start A Practice Test', width=20,
-           command=lambda: practice_test(root_window, dash_window, logged_in_user)).grid(row=0, column=0)
+           command=lambda: practice_test(root_window, dash_window, user)).grid(row=0, column=0)
     Label(dash_window, text='', width=20).grid(row=0, column=1)
     Button(dash_window, text='Start A Future Test', width=20).grid(row=0, column=2)
 
-    window_protocol.quit_confirmation(root_window, dash_window)
+    wp.quit_confirmation(root_window, dash_window)
 
 
-def practice_test(root_window, dash_window, logged_in_user):
+def practice_test(root_window, dash_window, user):
     dash_window.destroy()
-    practice_test_window.practice_test_window(root_window, logged_in_user)
+    pth.practice_test_window(root_window, user)
