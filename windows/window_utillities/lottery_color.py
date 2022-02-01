f4830d716d5e0e031806c0148161f81f161b0547
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-01 13:34:15 -0600

    LG-2: -Created function to return string color depending on lottery.

---
+++ windows/window_utillities/lottery_color.py
@@ -0,0 +1,11 @@
+def color(lottery):
+    if lottery == 'Powerball':
+        return 'red'
+    elif lottery == 'Mega Millions':
+        return 'gold'
+    elif lottery == 'Lotto America':
+        return 'yellow'
+    elif lottery == 'Cash 4 Life':
+        return 'green'
+    else:
+        return 'gold'
