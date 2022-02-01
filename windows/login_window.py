f532177e55b1b4080e83d174b941917278a7e500
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-01 04:58:22 -0600

    LG-2: -Optimized variable and import naming. -Organized code blocks.

--- windows/login_window.py
+++ windows/login_window.py
@@ -1,10 +1,10 @@
 from tkinter import *
-from tkinter import messagebox
-from storage import local_data_utilities
-from windows import dashboard_window
-from windows import registration_window
-from windows.window_utillities import window_icon
-from windows.window_utillities import window_protocol
+from tkinter import messagebox as m
+from storage import local_data_utilities as ldu
+from windows import dashboard_window as dw
+from windows import registration_window as rw
+from windows.window_utillities import window_icon as wi
+from windows.window_utillities import window_protocol as wp
 import tkinter
 
 
@@ -12,37 +12,44 @@ def login_window(root_window):
     log_window = tkinter.Toplevel(root_window)
     log_window.geometry('220x100')
     log_window.title('Login')
-    log_window.iconbitmap(window_icon.window_icon())
+    log_window.iconbitmap(wi.window_icon())
 
-    Label(log_window, text='User Name').grid(row=0, column=0)
-    Label(log_window, text='Password').grid(row=1, column=0)
+    user_label = Label(log_window, text='User Name')
+    pass_label = Label(log_window, text='Password')
 
     username = StringVar()
     password = StringVar()
 
-    Entry(log_window, textvariable=username).grid(row=0, column=1)
-    Entry(log_window, textvariable=password, show='*').grid(row=1, column=1)
+    user_entry = Entry(log_window, textvariable=username)
+    pass_entry = Entry(log_window, textvariable=password, show='*')
 
-    Button(log_window, text='Login', command=lambda: validate_login(username, password, root_window, log_window)).grid(
-        row=4,
-        column=0)
-    Button(log_window, text='Register',
-           command=lambda: registration_window.registration_window(root_window, log_window)).grid(row=4,
-                                                                                                  column=1)
+    login_button = Button(log_window, text='Login',
+                          command=lambda: validate_login(username, password, root_window, log_window))
+    register_button = Button(log_window, text='Register',
+                             command=lambda: rw.registration_window(root_window, log_window))
 
-    window_protocol.quit_confirmation(root_window, log_window)
+    user_label.grid(row=0, column=0)
+    pass_label.grid(row=1, column=0)
+    user_entry.grid(row=0, column=1)
+    pass_entry.grid(row=1, column=1)
+    login_button.grid(row=4, column=0)
+    register_button.grid(row=4, column=1)
+
+    wp.quit_confirmation(root_window, log_window)
 
 
-def validate_login(user_name, pass_word, root_window, log_window):
-    if user_name.get() == '' and pass_word.get() == '':
-        messagebox.showwarning('Empty Fields!', 'Enter a username and password to log in!')
+def validate_login(username, password, root_window, log_window):
+    user_name = username.get()
+    pass_word = password.get()
+    if user_name == '' and pass_word == '':
+        m.showwarning('Empty Fields!', 'Enter a username and password to log in!')
 
-    users = local_data_utilities.load_users()
+    users = ldu.load_users()
 
     for user in users:
-        if user.username == user_name.get():
-            if user.password != pass_word.get():
-                messagebox.showwarning('Wrong Password!', 'You have entered the wrong password, try again!')
+        if user.username == user_name:
+            if user.password != pass_word:
+                m.showwarning('Wrong Password!', 'You have entered the wrong password, try again!')
             else:
                 log_window.destroy()
                 dashboard(root_window, user)
@@ -50,4 +57,4 @@ def validate_login(user_name, pass_word, root_window, log_window):
 
 
 def dashboard(root_window, logged_in_user):
-    dashboard_window.dashboard_window(root_window, logged_in_user)
+    dw.dashboard_window(root_window, logged_in_user)
