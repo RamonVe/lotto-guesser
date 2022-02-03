f634bdc88afe4965bd0f989f69385de57b7e1726
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-03 16:24:07 -0600

    LG-3: -Implemented future test home window.

--- windows/dashboard_window.py
+++ windows/dashboard_window.py
@@ -1,5 +1,6 @@
 from entities import user as u
 from tkinter import *
+from windows.test_windows.future_test import future_test_home_window as fth
 from windows.test_windows.practice_test import practice_test_home_window as pth
 from windows.window_utillities import window_icon as wi
 from windows.window_utillities import window_protocol as wp
@@ -16,7 +17,8 @@ def dashboard_window(root_window, user):
     Button(dash_window, text='Start A Practice Test', width=20,
            command=lambda: practice_test(root_window, dash_window, user)).grid(row=0, column=0)
     Label(dash_window, text='', width=20).grid(row=0, column=1)
-    Button(dash_window, text='Start A Future Test', width=20).grid(row=0, column=2)
+    Button(dash_window, text='Start A Future Test', width=20,
+           command=lambda: future_test(root_window, dash_window, user)).grid(row=0, column=2)
 
     wp.quit_confirmation(root_window, dash_window)
 
@@ -24,3 +26,8 @@ def dashboard_window(root_window, user):
 def practice_test(root_window, dash_window, user):
     dash_window.destroy()
     pth.practice_test_window(root_window, user)
+
+
+def future_test(root_window, dash_window, user):
+    dash_window.destroy()
+    fth.future_test_window(root_window, user)
---
+++ windows/test_windows/future_test/future_test_home_window.py
@@ -0,0 +1,77 @@
+from tkinter import *
+from windows import dashboard_window as dw
+from windows.test_windows.practice_test import practice_test_input_window as pti
+from windows.window_utillities import window_icon as wi
+from windows.window_utillities import window_protocol as wp
+import tkinter
+
+
+def future_test_window(root_window, user):
+    future_window = tkinter.Toplevel(root_window)
+    future_window.geometry('320x150')
+    future_window.title('Future Test')
+    future_window.iconbitmap(wi.window_icon())
+
+    test_label = Label(future_window, text='Select a lottery to predict.')
+
+    power_ball_button = Button(future_window, text='Powerball',
+                               command=lambda: power_ball(root_window, future_window, user))
+
+    empty_space_one = Label(future_window, text='')
+
+    mega_millions_button = Button(future_window, text='Mega Millions',
+                                  command=lambda: mega_millions(root_window, future_window, user))
+    lotto_america_button = Button(future_window, text='Lotto America',
+                                  command=lambda: lotto_america(root_window, future_window, user))
+
+    empty_space_two = Label(future_window, text='')
+
+    cash_four_life_button = Button(future_window, text='Cash 4 Life',
+                                   command=lambda: cash_four_life(root_window, future_window, user))
+    tn_cash_button = Button(future_window, text='TN Cash',
+                            command=lambda: tn_cash(root_window, future_window, user))
+
+    dashboard_button = Button(future_window, text='Return To Dashboard',
+                              command=lambda: dash_board(root_window, future_window, user))
+
+    test_label.grid(row=0, column=1)
+    power_ball_button.grid(row=1, column=0)
+    empty_space_one.grid(row=2, column=0)
+    mega_millions_button.grid(row=3, column=0)
+    lotto_america_button.grid(row=1, column=2)
+    empty_space_two.grid(row=2, column=3)
+    cash_four_life_button.grid(row=3, column=2)
+    tn_cash_button.grid(row=4, column=1)
+    dashboard_button.grid(row=5, column=1)
+
+    wp.quit_confirmation(root_window, future_window)
+
+
+def power_ball(root_window, future_window, user):
+    practice_test_input(root_window, future_window, 'Powerball', user)
+
+
+def mega_millions(root_window, future_window, user):
+    practice_test_input(root_window, future_window, 'Mega Millions', user)
+
+
+def lotto_america(root_window, future_window, user):
+    practice_test_input(root_window, future_window, 'Lotto America', user)
+
+
+def cash_four_life(root_window, future_window, user):
+    practice_test_input(root_window, future_window, 'Cash 4 Life', user)
+
+
+def tn_cash(root_window, future_window, user):
+    practice_test_input(root_window, future_window, 'TN Cash', user)
+
+
+def practice_test_input(root_window, future_window, selected_lottery, user):
+    future_window.destroy()
+    pti.practice_session_item_guess(root_window, selected_lottery, user)
+
+
+def dash_board(root_window, future_window, user):
+    future_window.destroy()
+    dw.dashboard_window(root_window, user)
