dcbaa07f0e0ac4ed5b105d75198e2fec7a87aedb
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-04 23:54:21 -0600

    LG-3: -Renamed file name.

---
+++ lotto_guesser.py
@@ -0,0 +1,3 @@
+from windows import root_window
+
+root_window.initialize_ui()
--- main.py
+++
@@ -1,6 +0,0 @@
-from storage import local_data_utilities
-from windows import root_window
-
-root_window.initialize_ui()
-
-# local_data_utilities.delete_users()
