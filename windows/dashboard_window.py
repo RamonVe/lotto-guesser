edb90780294a0f62a955900819516ab0b23de5f8
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-05 01:36:35 -0600

    LG-3: -Improved look. -Added future test update frame, label, dropdown, and button.

--- windows/dashboard_window.py
+++ windows/dashboard_window.py
@@ -1,5 +1,6 @@
 from entities import user as u
 from tkinter import *
+from tkinter import ttk
 from windows.test_windows.future_test import future_test_home_window as fth
 from windows.test_windows.practice_test import practice_test_home_window as pth
 from windows.window_utillities import window_icon as wi
@@ -9,16 +10,34 @@ import tkinter
 
 def dashboard_window(root_window, user):
     dash_window = tkinter.Toplevel(root_window)
-    dash_window.geometry('450x200')
     first_name = u.User.first_name(user)
     dash_window.title(first_name + "'s Dashboard")
     dash_window.iconbitmap(wi.window_icon())
 
-    Button(dash_window, text='Start A Practice Test', width=20,
-           command=lambda: practice_test(root_window, dash_window, user)).grid(row=0, column=0)
-    Label(dash_window, text='', width=20).grid(row=0, column=1)
-    Button(dash_window, text='Start A Future Test', width=20,
-           command=lambda: future_test(root_window, dash_window, user)).grid(row=0, column=2)
+    parent_frame = LabelFrame(dash_window)
+    practice_frame = LabelFrame(parent_frame)
+    future_frame = LabelFrame(parent_frame)
+    saved_tests_frame = LabelFrame(dash_window)
+
+    practice_button = Button(practice_frame, text='Start A Practice Test', width=20,
+                             command=lambda: practice_test(root_window, dash_window, user), font='Times 12 bold')
+    future_button = Button(future_frame, text='Start A Future Test', width=20,
+                           command=lambda: future_test(root_window, dash_window, user), font='Times 12 bold')
+    saved_tests_label = Label(saved_tests_frame, text='Select a saved future test to update it.',
+                              font='Times 12 bold')
+    saved_tests_dropdown = ttk.Combobox(saved_tests_frame, values=['test', 'test2'], font='Times 12 bold')
+    update_button = Button(saved_tests_frame, text='Update', font='Times 12 bold')
+
+    parent_frame.pack()
+    practice_frame.pack(side='left', padx=10)
+    future_frame.pack(side='right', padx=10)
+    saved_tests_frame.pack(pady=20)
+
+    practice_button.pack()
+    future_button.pack()
+    saved_tests_label.pack()
+    saved_tests_dropdown.pack()
+    update_button.pack()
 
     wp.quit_confirmation(root_window, dash_window)
 
