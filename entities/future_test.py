89df3217fd1019c4dfbb7a63187f38fff3f4169d
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-04 23:45:27 -0600

    LG-3: -Created class to save future test data.

---
+++ entities/future_test.py
@@ -0,0 +1,26 @@
+class FutureTest:
+    def __init__(self, user, time, lottery_name, lottery_date, item_guesses, number_item_pair):
+        self.user = user.user_as_list(),
+        self.time = time
+        self.lottery_name = lottery_name,
+        self.lottery_date = lottery_date,
+        self.item_guesses = item_guesses,
+        self.number_item_pair = number_item_pair
+
+    def user(self):
+        return self.user
+
+    def time(self):
+        return self.time
+
+    def lottery_name(self):
+        return self.lottery_name
+
+    def lottery_date(self):
+        return self.lottery_date
+
+    def item_guesses(self):
+        return self.item_guesses
+
+    def number_item_pair(self):
+        return self.number_item_pair
