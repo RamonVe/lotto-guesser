de844edf01de20d4b5776b680dce3a087f567aa9
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-05 03:34:59 -0600

    LG-3: -Added functionality to load a user's saved future tests as dropdown options.

--- windows/dashboard_window.py
+++ windows/dashboard_window.py
@@ -1,3 +1,5 @@
+import re
+
 from entities import user as u
 from tkinter import *
 from tkinter import ttk
@@ -21,12 +23,20 @@ def dashboard_window(root_window, user):
 
     practice_button = Button(practice_frame, text='Start A Practice Test', width=20,
                              command=lambda: practice_test(root_window, dash_window, user), font='Times 12 bold')
+
     future_button = Button(future_frame, text='Start A Future Test', width=20,
                            command=lambda: future_test(root_window, dash_window, user), font='Times 12 bold')
+
     saved_tests_label = Label(saved_tests_frame, text='Select a saved future test to update.',
                               font='Times 12 bold')
-    saved_tests_dropdown = ttk.Combobox(saved_tests_frame, values=['test', 'test2'], font='Times 12 bold')
-    update_button = Button(saved_tests_frame, text='Update', font='Times 12 bold')
+
+    selected_future_test = StringVar()
+    saved_tests_dropdown = ttk.Combobox(saved_tests_frame, values=get_saved_future_tests(user),
+                                        textvariable=selected_future_test,
+                                        font='Times 12 bold')
+
+    update_button = Button(saved_tests_frame, text='Update', font='Times 12 bold',
+                           command=lambda: get_saved_future_tests(user))
 
     parent_frame.pack()
     practice_frame.pack(side='left', padx=10)
@@ -50,3 +60,19 @@ def practice_test(root_window, dash_window, user):
 def future_test(root_window, dash_window, user):
     dash_window.destroy()
     fth.future_test_window(root_window, user)
+
+
+def get_saved_future_tests(user):
+    f = open('storage/future_tests/' + user.username + '_future_test_storage.txt', 'r')
+    saved_tests = f.readlines()
+
+    saved_tests_dropdown_options = []
+    for tests in saved_tests:
+        split = tests.split(',')
+        lottery_info = split[4]
+        lottery_date = split[5]
+        lotto_name = re.findall('"([^"]*)"', lottery_info)
+        lotto_date = re.findall('"([^"]*)"', lottery_date)
+        saved_tests_dropdown_options.append(lotto_name[1] + ': ' + lotto_date[1])
+
+    return saved_tests_dropdown_options
