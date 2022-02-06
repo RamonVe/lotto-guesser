1664f8d3c217510004c1ee8db2e75d717ed08cbd
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-05 23:37:33 -0600

    LG-3: -Added functionality to also pass the index of the saved future test so that the index can be later used to update that future test.

--- windows/dashboard_window.py
+++ windows/dashboard_window.py
@@ -86,6 +86,7 @@ def prepare_future_test(user, selected_future_test):
     saved_tests = f.readlines()
 
     for test in saved_tests:
+        index = saved_tests.index(test)
         split_test = test.split(',')
         lottery_info = split_test[4]
         lottery_date = split_test[5]
@@ -96,7 +97,9 @@ def prepare_future_test(user, selected_future_test):
         test_details = [lotto_name[1], lotto_date[1]]
 
         if selected_test == test_details:
-            return test
+            index_test = [index, test]
+            print(index_test)
+            return index_test
 
 
 def submit(root_window, current_window, user, selected_future_test):
