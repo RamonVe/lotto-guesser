15642f3bb977325be9e8f37bb9194c2f08fa0053
RamonVe <98433288+RamonVe@users.noreply.github.com>
2022-02-08 23:15:07 -0600

    LG-7: -Reworked file to use json package only to save users to a textfile instead of pickle.

--- storage/local_data_utilities.py
+++ storage/local_data_utilities.py
@@ -1,40 +1,29 @@
 import json
-import pickle
+import os
 
 
 # This function saves a new user to a user list file via the pickle package.
 def save_user(new_user):
-    file_name = open('storage/user_list', 'rb')
-    loaded_file = pickle.load(file_name)
+    with open('storage/user_list.txt', 'a+') as file:
+        json_user = json.dumps(new_user.__dict__)
+        file.write(str(json_user) + '\n')
 
-    loaded_user_list = list(loaded_file)
-
-    print(loaded_user_list)
-
-    loaded_user_list.append(new_user)
-
-    with open('storage/user_list', 'wb') as s:
-        pickle.dump(loaded_user_list, s, protocol=pickle.HIGHEST_PROTOCOL)
-
-    print(loaded_user_list)
+        with open('storage/future_tests/' + new_user.username + '_future_test_storage.txt', 'a') as f:
+            f.write('')
 
 
 # This function loads the user list.
 def load_users():
-    file_name = open('storage/user_list', 'rb')
-    loaded_file = pickle.load(file_name)
+    f = open('storage/user_list.txt', 'a+')
+    users = f.readlines()
 
-    loaded_user_list = list(loaded_file)
-
-    return loaded_user_list
+    return users
 
 
 # This function deletes all saved users and creates a new user list file.
 # Currently, this function is not implemented.
 def delete_users():
-    new_user_list = []
-    with open('user_list', 'wb') as s:
-        pickle.dump(new_user_list, s, protocol=pickle.HIGHEST_PROTOCOL)
+    os.remove('storage/user_list.txt')
 
 
 # This function saves a future test for the logged-in user.
